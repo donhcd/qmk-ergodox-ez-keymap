9e3a5defcf7f7832e4888940bdfe8a1946d45e56
Donald Huang <don@sigmacomputing.com>
2025-05-28 16:40:08 -0400

    add more thumb keys

--- XlrwM/keymap.c
+++ XlrwM/keymap.c
@@ -111,8 +111,8 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_ergodo
   'R',           'R','R',           'R',           'R',        'R',
     'L',  'L',           'L',           'L',           'L',    'L',           'L',
   'R','R',           'R',           'R',       'R',         'R',       'R',
-    'L', 'L',         'L', 'L',    'L',
-  'R','R', 'R', 'R', 'R',
+    'L', 'L',         '*', '*',    '*',
+  '*','*', '*', 'R', 'R',
                                                                                                     '*',      '*',        '*',        '*',
                                                                                                                     '*', '*',
                                                                                     '*',       '*','*', '*',        '*','*'
